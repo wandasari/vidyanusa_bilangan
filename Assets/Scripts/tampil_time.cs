8092cf77b6493ef1e979bd404a445cbe6871e740
wandasari <novita.wandasari@gmail.com>
2016-11-16 15:58:34 +0700

    versi 1.0.0

---
+++ Assets/Scripts/tampil_time.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tampil_time : MonoBehaviour {
+	public float timer =0.0f;
+	public bool timerStarted = false;
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.N))
+			{
+				if (timerStarted==false)
+					timerStarted=true;
+				else
+					timerStarted=false;			
+			}
+
+			if(timerStarted)
+			timer+=Time.deltaTime;
+
+	}
+
+			void onGUI()
+			{
+				GUI.Label(new Rect (5,5,200,100), "Timer:" +timer);
+			}
+}
